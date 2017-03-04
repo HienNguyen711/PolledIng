e1ca0c743377cc3dbd9a9f8f4927bbf69cb67aa9
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 19:23:25 +0200

    add socket on front-end with react :100:

--- components/App.js
+++ components/App.js
@@ -1,6 +1,26 @@
 var React = require('react');
+var io = require('socket.io-client');
+
+var App = React.createClass({
+  //willMount
+  componentWillMount() {
+
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect',this.connect);
+  },
+
+  //connect()
+  connect() {
+    console.log('Connected from React: '+ this.socket.id);
+
+  }
+
+  render() {
+    return (
+      <h1>Hello React </h1>
+    )
+  }
 
 
-
-
-var App = React.createClass
+});
+module.exports = App;
