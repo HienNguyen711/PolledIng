5bb671997ff98e54f4ff4a0a8c31fa899640b549
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 20:00:16 +0200

    create Form component for audience to join

---
+++ components/common/Join.js
@@ -0,0 +1,36 @@
+var React = require('react');
+
+
+
+
+
+var Join = React.createClass({
+
+
+
+  join() {
+    var memberName = React.findDOMNode(this.refs.name).value;
+    alert('');
+
+  },
+
+
+  render() {
+    return (
+      <form>
+
+
+
+
+
+      </form>
+
+
+    )
+
+
+  }
+
+
+
+});
