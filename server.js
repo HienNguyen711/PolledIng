209e02c45021442c78544da7203e795976a20433
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 19:19:19 +0200

    incorporate socket.io on server

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 
 
 
-
-
 var port = process.env.PORT || 3000;
 var app= express();
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(port,function(){
-  console.log('Running on port '+port);
-})
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+  console.log('Connected: '+ socket.id);
+});
+
+
+console.log('Running on port '+port);
