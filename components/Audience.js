63f1e42d6ed82084f65f45c3a1bbe484d9bdf7bb
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 19:56:33 +0200

    ...

--- components/Audience.js
+++ components/Audience.js
@@ -1,9 +1,15 @@
 
 var React = require('react');
+var Display = require('./common/Display');
 var Audience = React.createClass({
   render() {
     return (
-      <h1>Audience: {this.props.status}</h1>
+      <div>
+        <Display if={this.props.status ==='connected'}/>
+          <h1>Join the session</h1>
+
+
+      </div>
     )
   }
 
